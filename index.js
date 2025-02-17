c46e93b7805ed8961f289a79150981c7f924aca8
Amal-els <amalbahri19@gmail.com>
2025-02-17 15:51:34 +0100

    latest commit

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-
-bouton = document.querySelector(".btn");
 function removeTask(b){
     const selectedItem = b.parentNode;
     selectedItem.remove();
 }
 
+function addElement(e,cl){
+    const d = document.createElement(e);
+    d.classList.add(cl);
+    return d;
+}
+
 function addTask(){
+    n = document.querySelector("#name");
+    c = document.querySelector("#content");
+    nom = n.value;
+    contenu = c.value;
+    if(nom == ""||contenu == ""){
+        alert("Entrée non valide!");
+    }
+    else{
+    const cont = document.querySelector(".container1");
+    const newDiv = addElement("div","added-item");
+    const textNode = addElement("p","text_el");
+    const icon = addElement("img","icon");
+    icon.src="images/trash-can.png";
+    icon.width="15";
+    icon.height="15";
+    icon.style.marginTop="1px";
+    icon.style.marginLeft="10px";
+    textNode.textContent = nom+' : '+contenu;
+    const closeButton = addElement("button","close");
+    closeButton.appendChild(icon);
+    closeButton.addEventListener("click",()=>removeTask(closeButton));
+    newDiv.appendChild(textNode);
+    newDiv.appendChild(closeButton);    
+    cont.appendChild(newDiv);
+    }
+}
+
+bouton = document.querySelector(".btn");
+bouton.addEventListener("click",()=>addTask());
+
+/*function addTask(){
     n = document.querySelector("#name");
     c = document.querySelector("#content");
     nom =n.value;
@@ -33,8 +68,9 @@ function addTask(){
     newDiv.appendChild(closeButton);    
     cont.appendChild(newDiv);
 }
-}
-bouton.addEventListener("click",()=>addTask());
+}*/
+
+
 
 
 
